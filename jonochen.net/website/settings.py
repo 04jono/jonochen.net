1069bfbc6f81310c42fdc9f967cc9f5e87f26b59
Jonathan Chen <jqc3@cornell.edu>
2024-04-21 01:38:21 -0400

    typo

--- jonochen.net/website/settings.py
+++ jonochen.net/website/settings.py
@@ -36,7 +36,7 @@ else:
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('DJANGO_CSRF_TRUSTED_ORIGINS', '127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '127.0.0.1').split(',')
 
 # Application definition
 
