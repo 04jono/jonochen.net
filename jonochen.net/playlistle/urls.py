5cc362faa8350e2f5d77c3fddc96304b7fc04642
Jonathan Chen <jqc3@cornell.edu>
2024-04-24 23:46:55 -0400

    added urls

--- jonochen.net/playlistle/urls.py
+++ jonochen.net/playlistle/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.playlistle, name='playlistle'),
     path("song_submit", views.submit_song, name='submit_song'),
     path("song_clip", views.get_clipped_song, name='get_clipped_song'),
+    path("song_hash", views.get_song_hash, name='get_song_hash'),
+    path("song_identifiers", views.get_all_song_identifiers, name='get_all_song_identifiers'),
 ]
