108789af5fa15a2024fa670496b8c68324288068
Jonathan Chen <jqc3@cornell.edu>
2024-05-12 22:55:31 -0400

    date fix

--- scripts/reset_daily.py
+++ scripts/reset_daily.py
@@ -26,7 +26,7 @@ def reset_daily():
             latest_song = cursor.fetchone()
             last_date = latest_song[0]
 
-            if last_date.date() != datetime.datetime.now().date():
+            if  datetime.datetime.fromtimestamp(last_date) != datetime.datetime.now().date():
                 cursor.execute("SELECT * FROM playlistle_song ORDER BY RANDOM() LIMIT 1")
                 random_song = cursor.fetchone()
                 insert_query = 'INSERT INTO playlistle_songofday (date_added, song_id) VALUES (%s, %s)'
