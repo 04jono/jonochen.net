a5aaead01f7833878c2e3d630c41b1561e0e604c
Jonathan Chen <jqc3@cornell.edu>
2024-05-12 22:21:52 -0400

    bug fix for mobile and auto update

---
+++ scripts/reset_daily.py
@@ -0,0 +1,48 @@
+import psycopg2
+import os
+import datetime
+from psycopg2 import Error
+import time
+
+def reset_daily():
+    while True:
+        try:
+            connection = psycopg2.connect(user = os.getenv('POSTGRES_USER'),
+                                            password = os.getenv('POSTGRES_PASSWORD'),
+                                            host = os.getenv('POSTGRES_HOST'),
+                                            port = os.getenv('POSTGRES_PORT'),
+                                            database = os.getenv('POSTGRES_DB'))
+
+            cursor = connection.cursor()
+            # Print PostgreSQL Connection properties
+            print ( connection.get_dsn_parameters(),"\n")
+
+            # Print PostgreSQL version
+            cursor.execute("SELECT version();")
+            record = cursor.fetchone()
+            print("You are connected to - ", record,"\n")
+
+            cursor.execute("SELECT * FROM playlistle_songofday ORDER BY date_added DESC LIMIT 1")
+            latest_song = cursor.fetchone()
+            last_date = latest_song[0]
+
+            if last_date != datetime.date.today():
+                cursor.execute("SELECT * FROM playlistle_song ORDER BY RANDOM() LIMIT 1")
+                random_song = cursor.fetchone()
+                insert_query = 'INSERT INTO playlistle_songofday (date_added, song_id) VALUES (%s, %s)'
+                cursor.execute(insert_query, (datetime.date.today(), random_song[6]))
+                connection.commit()
+                print(f"Song of the day {random_song[6]} inserted successfully")
+
+        except (Exception, Error) as error :
+            print ("Error while connecting to PostgreSQL", error)
+        finally:
+            #closing database connection.
+            if(connection):
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed")
+        time.sleep(15 * 60)
+
+if __name__ == '__main__':
+    reset_daily()
