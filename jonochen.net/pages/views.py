3242f855bcd245310ce5cfcf79d407e6b20de433
Jonathan Chen <04jono@gmail.com>
2024-08-21 20:54:19 -0400

    disable resume

--- jonochen.net/pages/views.py
+++ jonochen.net/pages/views.py
@@ -24,15 +24,18 @@ def home(request):
     return render(request, "pages/home.html")
 
 def resume(request):
-    file_path = os.path.join(settings.BASE_DIR, 'pages/files/resume/resume.pdf')
+    
+    # file_path = os.path.join(settings.BASE_DIR, 'pages/files/resume/resume.pdf')
 
-    # Open the PDF file in binary mode
-    if os.path.exists(file_path):
-        pdf_file = open(file_path, 'rb')
-        response = FileResponse(pdf_file, content_type='application/pdf')
-        response['Content-Disposition'] = 'filename="resume.pdf"'
-        return response
-    else:
-        return HttpResponseNotFound("No Resume file found")
+    # # Open the PDF file in binary mode
+    # if os.path.exists(file_path):
+    #     pdf_file = open(file_path, 'rb')
+    #     response = FileResponse(pdf_file, content_type='application/pdf')
+    #     response['Content-Disposition'] = 'filename="resume.pdf"'
+    #     return response
+    # else:
+    #     return HttpResponseNotFound("No Resume file found")
+    
+    return render(request, "pages/home.html")
     
 
