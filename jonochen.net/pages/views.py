1d1c5760751c5770db8b7c56388060098ee4f133
Jonathan Chen <04jono@gmail.com>
2025-01-03 18:04:07 -0500

    homepage modifications

--- jonochen.net/pages/views.py
+++ jonochen.net/pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import FileResponse, HttpResponseNotFound, JsonResponse, HttpResponse
+from django.http import FileResponse, HttpResponseNotFound, JsonResponse, HttpResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_protect
 from django.conf import settings
 from django.views.decorators.http import require_GET
@@ -16,6 +16,8 @@ User-agent: GPTBot
 Disallow: /
 """
 
+ALLOW_RESUME = False
+
 @require_GET
 def robots(request):
     return HttpResponse(ROBOTS_TXT, content_type="text/plain")
@@ -24,6 +26,8 @@ def home(request):
     return render(request, "pages/home.html")
 
 def resume(request):
+    if not ALLOW_RESUME:
+        return HttpResponseForbidden("Resume disabled")
     
     file_path = os.path.join(settings.BASE_DIR, 'pages/files/resume/resume.pdf')
 
